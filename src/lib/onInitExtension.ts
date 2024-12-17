649cdb179c9e1528c5805ce0a978a4a097149cdc
khoa <khoa@vietswiss.com>
2024-12-17 17:52:48 +0700

    refactor: update build log

--- src/lib/onInitExtension.ts
+++ src/lib/onInitExtension.ts
@@ -1,8 +1,8 @@
 import type { Payload } from 'payload'
 
-import type { MyPluginOptions } from '../types.js'
+import type { FormLabelPluginOptions } from '../types.js'
 
-export const onInitExtension = (pluginOptions: MyPluginOptions, payload: Payload): void => {
+export const onInitExtension = (pluginOptions: FormLabelPluginOptions, payload: Payload): void => {
   try {
     payload.logger.info({ msg: 'Hello from onInitExtension' })
   } catch (err: unknown) {
